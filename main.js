d1254c05759e670a2e98fe172318218602776c53
Mark Ilchenko <markilchenko18.hillel@gmail.com>
2023-01-17 20:51:10 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,186 @@
+// Homework 1
+
+// let x=10;
+//
+// for (let i = 11; i <= 20; i = i + 1) {
+//     x = x + ', ' + i;
+// }
+//
+// console.log(x)
+
+
+
+
+// Homework 2
+// let x='';
+//
+// for (let i = 10; i <= 20; i = i + 1) {
+//     console.log(`sqrt ${i} = ${i * i}`);
+// }
+
+
+
+
+
+// Homework 3
+
+// let x='';
+//
+// for (let i = 1; i <= 10; i = i + 1) {
+//     console.log(`multiplication ${i} = ${i * 7}`);
+// }
+
+
+
+
+
+// Homework 4
+
+// let sum = 0;
+//
+// for (let i = 1; i <= 15; i = i +1) {
+//     sum = sum + i;
+// }
+//
+// console.log(sum);
+
+
+
+
+// Homework 5
+
+// let sum = 0;
+//
+// for (let i = 15; i <= 35; i = i + 1) {
+//     sum = sum + i;
+// }
+//
+// console.log(sum);
+
+
+
+
+
+// Homework 6
+
+// let sum = 0;
+//
+// for (let i = 1; i <= 500; i = i + 1) {
+//     sum = sum + i;
+// }
+//
+// console.log(sum / 500)
+
+
+
+// Homework 7
+// let sum = 0;
+//
+// for (let i = 30; i <= 80; i = i + 1) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//         sum = sum + i;
+//     }
+// }
+//
+// console.log(sum)
+
+
+
+// Homework 8
+// let sum = 0;
+//
+// for (let i = 100; i <= 200; i = i + 1) {
+//     if (i % 3 === 0) {
+//         console.log(i);
+//     }
+// }
+
+
+// Homework 9
+
+// let n = prompt("Enter your number");
+// for (let i=1;i<=n;i++) {
+//     if (n%i==0) {
+//         console.log(`The divisor of your number is: ` + i);
+//     }
+// }
+
+
+
+
+
+// Homework 10
+
+
+// let n = prompt("Enter your number");
+// for (let i = 1;i <= n; i++) {
+//     if (n % i === 0 ) {
+//         console.log(n)
+//     }
+// }
+
+
+
+
+
+
+// Homework 11
+
+// let sum = 0;
+// let n = prompt("Enter your number");
+// for (let i = 1;i <= n; i++) {
+//     if (n % i === 0) {
+//         console.log(i);
+//         sum = sum + i;
+//     }
+// }
+//
+// console.log('Сумма ваших парних чисел: ' + sum)
+
+
+
+
+
+
+// Homework 12
+
+// let res = " x\t 1\t 2\t 3\t 4\t 5\t 6\t 7\t 8\t 9\t10"
+// for (let i = 1; i <= 10; i++) {
+//     res += `\n${i < 10 ? ' ' + i : i}`
+//     for (let j = 1; j <= 10; j++) {
+//         res += `\t${i * j >= 10 ? i*j : ' ' + i*j}`
+//     }
+// }
+// console.log(res)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
